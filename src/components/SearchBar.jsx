f1cb13ba146cd9bb24a987fa38aad4a65bfc17fa
Vonney <davonnejv@gmail.com>
2024-01-09 16:07:10 -0600

    adding useRef hook for handling search

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,15 +1,25 @@
 import { Form } from "react-bootstrap";
+import { useRef } from "react";
 
 export default function SearchBar() {
+  const searchInput = useRef(null);
+
+  //handle search
+  const handleSearch = (e) => {
+    e.preventDefault();
+    console.log(searchInput.current.value);
+  };
+
   return (
     <div className="flex flex-col justify-center items-center h-screen">
       <h1 className="mb-10 text-2xl font-serif">Image Search</h1>
       <div className="search-section">
-        <Form>
+        <Form onSubmit={handleSearch}>
           <Form.Control
             type="search"
             placeholder="Search For"
             className="w-96 outline-double"
+            ref={searchInput}
           />
         </Form>
       </div>
